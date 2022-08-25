88839c31a21e0d9fe561df461ffa1cb73ac80b7e
teacher PH2021 <postnikov_s@teacher.itstep.by>
2022-08-25 20:56:46 +0300

    connected to postgres

--- archivarious/settings.py
+++ archivarious/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+sl(d31d%@$2zas=vvea(s$*u4cs92o#0*3c-jh6_z(&*=ad7v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'archivarious.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER' : 'django',
+        'PASSWORD': 'django',
+        'HOST': 'localhost',
+        'PORT': '5555'
     }
 }
 
